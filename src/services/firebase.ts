4c9a175b8850c94b5386d27e038cf7336a37ec70
yyyerin <hyr0208@naver.com>
2026-01-23 14:12:18 +0900

    fix: Firebase Config 하드코딩 적용 (Jenkins 배포 환경변수 대응)

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCZpTQJcyAoUyZjEfGPYRzHXjhU2jRbRI0",
+  authDomain: "jiwonnow.firebaseapp.com",
+  projectId: "jiwonnow",
+  storageBucket: "jiwonnow.firebasestorage.app",
+  messagingSenderId: "940215224471",
+  appId: "1:940215224471:web:9820ed552bda22d50201d2",
+  measurementId: "G-CTXQCTCZGN",
 };
 
 const app = initializeApp(firebaseConfig);
